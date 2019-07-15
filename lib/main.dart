e267dc2f2398cd7abf91bc38ea493c41025fd2f4
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-07-15 12:17:59 +0530

    Made Baseic layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,266 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Color.fromRGBO(45, 47, 65, 1),
+        child: Padding(
+          padding: EdgeInsets.only(top: 25),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * .24,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                    ),
+                    InkWell(
+                      child: Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 100),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(35)),
+                        color: Color.fromRGBO(36, 38, 52, 1),
+                      ),
+                      height: MediaQuery.of(context).size.width * .24,
+                      width: MediaQuery.of(context).size.width * .24,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Icon(
+                              Icons.timelapse,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Clock',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(35)),
+                      ),
+                      height: MediaQuery.of(context).size.width * .24,
+                      width: MediaQuery.of(context).size.width * .24,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Icon(
+                              Icons.alarm,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Alarm',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(35)),
+                      ),
+                      height: MediaQuery.of(context).size.width * .24,
+                      width: MediaQuery.of(context).size.width * .24,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Icon(
+                              Icons.av_timer,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Timer',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(35)),
+                      ),
+                      height: MediaQuery.of(context).size.width * .24,
+                      width: MediaQuery.of(context).size.width * .24,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Icon(
+                              Icons.timer,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Stopwatch',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              VerticalDivider(
+                color: Colors.white,
+                width: 0, //TODO: Change 1 to 0
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.76,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
+                      ),
+                      Text(
+                        'Clock',
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 90),
+                      ),
+                      Text(
+                        '19:15',
+                        style: TextStyle(fontSize: 55, color: Colors.white),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                      ),
+                      Text(
+                        'Fri, 5 Jul',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Clock(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 40),
+                      ),
+                      Text(
+                        'Timezone',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Image(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                'https://cdn.countryflags.com/download/india/flag-svg.svg'),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 20),
+                          ),
+                          Text(
+                            'UTC+05:30 (IST)',
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 7),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Divider(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Automatic timezone',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Switch.adaptive(
+                            value: true,
+                            onChanged: (v) {},
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Clock extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      fallbackHeight: 250,
     );
   }
 }
